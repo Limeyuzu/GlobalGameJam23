2bfd3e25a31403b42acfda3c53f7be1394a24088
AppleSeed Smoothie <blake2704@outlook.com>
2023-02-04 12:40:27 +1300

    fixup lawny mowy

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -6,4 +6,5 @@ public class Globals
 {
     public static readonly int gridSizeX = 10;
     public static readonly int gridSizeY = 10;
+    public static readonly float gridSpacing = 1f;
 }
--- Assets/Scripts/Lawnmower.cs
+++ Assets/Scripts/Lawnmower.cs
@@ -1,4 +1,4 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -16,66 +16,72 @@ public class Lawnmower : MonoBehaviour
 
     private float yPos;
 
-    LevelGrid levelGrid;
-
     private float countDown;
 
-    private float speed;
+    private float speed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        levelGrid = GetComponent<LevelGrid>();
         direction = (Direction)(int)Random.Range(0, 3);
+        Debug.Log(direction.ToString());
+        transform.rotation.Set(0f, 0f, 0f, 1f);
         switch (direction)
         {
             case Direction.North:
-                xPos = Random.Range(0, levelGrid.width);
+                xPos = -Random.Range(0, Globals.gridSizeX - 1);
                 yPos = -1;
                 break;
             case Direction.South:
-                xPos = Random.Range(0, levelGrid.width);
-                yPos = levelGrid.width + 1;
+                xPos = -Random.Range(0, Globals.gridSizeX - 1);
+                yPos = Globals.gridSizeY + 1;
+                transform.Rotate(0f, 180.0f, 0f);
                 break;
             case Direction.East:
-                xPos = levelGrid.height + 1;
-                yPos = Random.Range(0, levelGrid.width);
+                xPos = -Globals.gridSizeX - 1;
+                yPos = Random.Range(0, Globals.gridSizeY);
+                transform.Rotate(0f, 90.0f, 0f);
                 break;
             case Direction.West:
-                xPos = -1;
-                yPos = Random.Range(0, levelGrid.width);
+                xPos = 1;
+                yPos = Random.Range(0, Globals.gridSizeY);
+                transform.Rotate(0f, -90.0f, 0f);
                 break;
 
         }
-        countDown = 5f;
-        speed = 1f;
+        countDown = 1f;
+        transform.position = new Vector3(xPos, 0.0f, yPos);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(countDown > 0)
+        if (countDown > 0)
         {
             countDown -= Time.deltaTime;
-        } else
+        }
+        else
         {
-            switch(direction)
+            switch (direction)
             {
                 case Direction.North:
                     yPos += speed * Time.deltaTime;
+                    if (yPos > Globals.gridSizeY) Start();
                     break;
                 case Direction.South:
                     yPos -= speed * Time.deltaTime;
+                    if (yPos < 0) Start();
                     break;
                 case Direction.East:
                     xPos += speed * Time.deltaTime;
+                    if (xPos > 0) Start();
                     break;
                 case Direction.West:
                     xPos -= speed * Time.deltaTime;
+                    if (xPos < Globals.gridSizeX) Start();
                     break;
-
             }
+            transform.position = new Vector3(xPos, 0.0f, yPos);
         }
     }
 }
-*/
