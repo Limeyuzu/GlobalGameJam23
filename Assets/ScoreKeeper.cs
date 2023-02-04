45ce8719e00a037da578b44ab6645fcaec8a4ec5
LemonFizz2814 <mathewlee2010@hotmail.com>
2023-02-04 21:41:18 +1300

    Display Score HUD

--- Assets/ScoreKeeper.cs
+++ Assets/ScoreKeeper.cs
@@ -1,18 +1,21 @@
 using Assets.GenericTools.Event;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreKeeper : MonoBehaviour
 {
     int playerScore = 0;
+    public TextMeshProUGUI score;
     // Start is called before the first frame update
     void Start()
     {
         EventManager.Subscribe(GameEvent.ScoreIncremented, (amount) =>
         {
             playerScore += (int)amount;
-            print(playerScore);
+            score.text = $"SCORE: {playerScore}";
         });
     }
 
