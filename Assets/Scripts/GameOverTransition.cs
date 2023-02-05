b8f33aaff27bfa9c37e20172db2904dffe24207f
Limeyuzu <miiyusli@gmail.com>
2023-02-05 16:55:09 +1300

    Add restart button on death

--- Assets/Scripts/GameOverTransition.cs
+++ Assets/Scripts/GameOverTransition.cs
@@ -1,6 +1,7 @@
 using Assets.GenericTools.Event;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameOverTransition : MonoBehaviour
@@ -8,15 +9,32 @@ public class GameOverTransition : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var blackScreen = GetComponentInChildren<Image>();
+        var images = GetComponentsInChildren<Image>();
         var gameOverText = GetComponentInChildren<TextMeshProUGUI>();
-        blackScreen.canvasRenderer.SetAlpha(0);
+        var resetButton = GetComponentInChildren<Button>();
+        foreach (var image in images)
+        {
+            image.canvasRenderer.SetAlpha(0);
+            image.color = new Color(image.color.r, image.color.b, image.color.g, 0);
+        }
         gameOverText.canvasRenderer.SetAlpha(0);
+        resetButton.enabled = false;
 
         EventManager.Subscribe(GameEvent.GameOver, _ =>
         {
-            blackScreen.CrossFadeAlpha(1, 3, false);
+            foreach (var image in images)
+            {
+                image.CrossFadeAlpha(1, 3, false);
+                image.color = new Color(image.color.r, image.color.b, image.color.g, 1);
+            }
             gameOverText.CrossFadeAlpha(1, 8, false);
+            resetButton.enabled = true;
         });
+    }
+
+    public void OnButtonPress()
+    {
+        // reset scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
