03a674d75f0dae2dc617c78aec75f4f0e442a5ab
Limeyuzu <miiyusli@gmail.com>
2023-02-05 13:59:51 +1300

    Rotate weeds randomly

--- Assets/Scripts/Garden/WeedRenderer.cs
+++ Assets/Scripts/Garden/WeedRenderer.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class WeedRenderer : MonoBehaviour
 {
+    [SerializeField] int RandomRotationDegrees = 15;
+    [SerializeField] float RandomScaleUp = 0.3f;
+
     private GameObject _weedModel1;
     private GameObject _weedModel2;
     private GameObject _weedModel3;
@@ -14,6 +17,15 @@ public class WeedRenderer : MonoBehaviour
         _weedModel1 = transform.Find("WeedSmall").gameObject;
         _weedModel2 = transform.Find("WeedMedium").gameObject;
         _weedModel3 = transform.Find("WeedLarge").gameObject;
+        RandomiseTransform(_weedModel1.transform);
+        RandomiseTransform(_weedModel2.transform);
+        RandomiseTransform(_weedModel3.transform);
+    }
+
+    private void RandomiseTransform(Transform t)
+    {
+        t.rotation = Quaternion.Euler(0, Random.Range(-RandomRotationDegrees, RandomRotationDegrees), 0);
+        t.localScale = new Vector3(t.localScale.x, t.localScale.y + RandomScaleUp * Random.value, t.localScale.z);
     }
 
     // Update is called once per frame
